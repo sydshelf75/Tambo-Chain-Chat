76e27466ca23f674bee81797fb387dd77038671b
syd <sridhar.sing@shelfexecution.com>
2026-02-08 01:59:44 +0530

    remove duplicate things

--- src/components/tambo/message-thread-full.tsx
+++ src/components/tambo/message-thread-full.tsx
@@ -8,7 +8,6 @@ import {
   MessageInputMcpPromptButton,
   MessageInputMcpResourceButton,
   MessageInputDictationButton,
-  MessageInputDictationButton,
   MessageInputSubmitButton,
   MessageInputTextarea,
   MessageInputToolbar,
@@ -19,8 +18,6 @@ import {
   MessageSuggestionsStatus,
 } from "@/components/tambo/message-suggestions";
 import { ScrollableMessageContainer } from "@/components/tambo/scrollable-message-container";
-
-import { ChatExportMenu } from "@/components/tambo/chat-export";
 
 import { ChatExportMenu } from "@/components/tambo/chat-export";
 import { ThreadContainer, useThreadContainerContext } from "./thread-container";
